66b2e4bac0c394375aa2ddd0c4c24d13ba70bbae
Manan Gupta <gmanan3222@gmail.com>
2025-06-19 19:07:21 +0530

    Time: 24 ms (40.7%), Space: 45.1 MB (25.72%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
@@ -1,16 +1,16 @@
 class Solution {
     public int minimumDeleteSum(String s1, String s2) {
-        int[][] dp = new int[s1.length()+1][s2.length()+1];
         
-        dp[s1.length()][s2.length()] = 0;
-        for(int i=s1.length(); i>= 0; i--){
-            for(int j=s2.length(); j>=0; j--){
-                if(i == s1.length() && j == s2.length()) continue;
-
-                if(i == s1.length()){
+        int len1 = s1.length();
+        int len2 = s2.length();
+        int[][] dp = new int[len1+1][len2+1];
+        for(int i=len1; i>= 0; i--){
+            for(int j=len2; j>=0; j--){
+                if(i == len1 && j == len2) dp[i][j] = 0;
+                else if(i == len1){
                     dp[i][j] = s2.charAt(j) + dp[i][j+1];
                 } 
-                else if(j == s2.length()){
+                else if(j == len2){
                     dp[i][j] = s1.charAt(i) + dp[i+1][j];
                 } else{
                     if(s1.charAt(i) == s2.charAt(j)) dp[i][j] = dp[i+1][j+1];
@@ -19,30 +19,5 @@ class Solution {
             }
         }
         return dp[0][0];
-    }
-
-    private int recur(int i, int j, String s1, String s2, int[][] dp){
-        
-        if(dp[i][j] != -1) return dp[i][j];
-
-        if(i == s1.length()){
-            return dp[i][j] = calcAscii(s2.substring(j));
-        }
-        if(j == s2.length()){
-            return dp[i][j] = calcAscii(s1.substring(i));
-        }
-
-        if(s1.charAt(i) == s2.charAt(j)) return dp[i][j] = recur(i+1, j+1, s1, s2, dp);
-        else return dp[i][j] = Math.min(s2.charAt(j)+recur(i, j+1, s1, s2, dp),
-                             s1.charAt(i)+recur(i+1, j, s1, s2, dp));
-
-    }
-
-    private int calcAscii(String s){
-        int total = 0;
-        for(int i=0; i<s.length(); i++){
-            total += s.charAt(i);
-        }
-        return total;
     }
 }
