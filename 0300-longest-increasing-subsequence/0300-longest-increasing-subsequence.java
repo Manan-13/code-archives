a9fafa3473d2073abc77536e704433132fb9a074
Manan Gupta <gmanan3222@gmail.com>
2025-06-21 13:42:55 +0530

    Time: 67 ms (26.67%), Space: 44.5 MB (47.34%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,22 +1,33 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[][] dp = new int[nums.length+1][nums.length+1];
-        for(int[] arr: dp) Arrays.fill(arr, -1);
-
-        // for(int i=0; i<nums.length; i++) dp[nums.length][i] = 0;
-        return recur(0, nums.length, nums, dp);
-
-    }
-
-    int recur(int index, int prev, int[] nums, int[][] dp){
+        // int[][] dp = new int[nums.length+1][nums.length+1];
+        int[] cur = new int[nums.length+1];
+        int[] next = new int[nums.length+1];
         
-        if(index == nums.length) return 0;
-        if(dp[index][prev] != -1) return dp[index][prev];
-        int notpick = recur(index+1, prev, nums, dp);
+        for(int i=nums.length-1; i>=0; i--){
+            for(int j=i-1; j>= -1; j--){
+                int notpick = next[j+1];
+                int pick = 0;
+                if(j == -1 || nums[i] > nums[j]) pick = 1 + next[i+1];
 
-        int pick = 0;
-        if(prev == nums.length || nums[index] > nums[prev]) pick = 1+recur(index+1, index, nums, dp);
+                cur[j+1] = Math.max(notpick, pick);
+            }
+            next = cur;
+        }
 
-        return dp[index][prev]=Math.max(notpick, pick);
+        return cur[0];
+
     }
+
+    // int recur(int index, int prev, int[] nums, int[][] dp){
+        
+    //     if(index == nums.length) return 0;
+    //     if(dp[index][prev+1] != -1) return dp[index][prev+1];
+    //     int notpick = recur(index+1, prev, nums, dp);
+
+    //     int pick = 0;
+    //     if(prev == -1 || nums[index] > nums[prev]) pick = 1+recur(index+1, index, nums, dp);
+
+    //     return dp[index][prev+1] = Math.max(notpick, pick);
+    // }
 }
